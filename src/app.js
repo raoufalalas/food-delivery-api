e4d57d909d9e3dc09c38ebc8c62a715e4cc498c2
Ra2of <114357776+raoufalalas@users.noreply.github.com>
2026-05-10 02:37:54 +0000

    Day 1: project setup - express server + folder structure

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors    = require('cors');
+const helmet  = require('helmet');
+const morgan  = require('morgan');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: '🍕 Food Delivery API is running!' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+module.exports = app;
